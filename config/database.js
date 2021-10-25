11df079aac2606e0272c7960311e22d0b9c2cbf5
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-10-25 11:07:10 -0400

    database

---
+++ config/database.js
@@ -0,0 +1,18 @@
+
+
+module.exports = {
+  url:
+    "mongodb+srv://KW1234:test1234@cluster0.jcysh.mongodb.net/roulette?retryWrites=true&w=majority"
+
+};
+
+
+
+// // config/database.js
+// const username  = 'Mary'
+// const password  = 'BZxn35BzacSsb.!'
+// const dbName    = 'roulette'
+
+// module.exports = {
+//     'url' : `mongodb+srv://${username}:${password}@taskmanagercluster.aqyy0.mongodb.net/${dbName}?retryWrites=true&w=majority`};
+// //mongodb+srv://Mary:<password>@taskmanagercluster.aqyy0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
